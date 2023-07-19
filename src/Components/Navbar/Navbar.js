5806e1b2351b2c9c38b1b12a1978204e21151605
Nikhil  Jagtap <nikhil.jagtap@automationedge.com>
2023-07-19 17:40:00 +0530

    footer component

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import logoImg from './valuedx_logo.png';
 function Navbar({onLogout, showLogoutButton}) {
     return(
 
- <div style={{display:"flex", alignItems: 'center',justifyContent:'space-between',backgroundColor:'black', height:50,padding:5}}>
+ <div className="footer1" style={{display:"flex", alignItems: 'center',justifyContent:'space-between', height:50,padding:5}}>
     <div>
     <img src={logoImg} width="100px" height="auto"/>
     </div>
