256c7605fe799c1d64c03d3857de431d6cda1679
Nikhil  Jagtap <nikhil.jagtap@autoedgecorp.com>
2023-08-07 15:29:32 +0530

    registration done

--- src/Components/Registration/RegistrationForm.js
+++ src/Components/Registration/RegistrationForm.js
@@ -1,76 +1,116 @@
-import React from 'react';
+//import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import './RegistrationForm.css';
 
-
 const RegistrationForm = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-  const onSubmit = (data) => {
-    // Handle form submission here, you can access the form data using the 'data' object.
-    console.log(data);
+  const [registrationMessage, setRegistrationMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const onSubmit = async (data) => {
+    console.log('Data being sent to the server:', data); // Add this line
+    try {
+      const response = await fetch('http://localhost:3001/api/addUser', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+  
+      if (response.ok) {
+        console.log('User data added successfully');
+        setRegistrationMessage('Registration Successful');
+        reset();
+
+        // Clear the message after 2 seconds
+        setTimeout(() => {
+          setRegistrationMessage('');
+        }, 2000);
+
+      } else {
+        console.error('Failed to add user data. Server returned:', response.status);
+        setErrorMessage("Please enter another valid Username");
+
+        setTimeout(() => {
+          setErrorMessage('');
+        }, 2000);
+
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
-
+  
   return (
     <div className="registration-form-container">
       <form className="registration-form" onSubmit={handleSubmit(onSubmit)}>
+        {/* First Name */}
         <div className="form-group">
-          <label>First Name</label>
+          <label htmlFor="firstName">First Name</label>
           <input
             type="text"
+            id="firstName"
             name="firstName"
             {...register('firstName', { required: true })}
           />
           {errors.firstName && <span className="error">This field is required</span>}
         </div>
 
+        {/* Last Name */}
         <div className="form-group">
-          <label>Last Name</label>
+          <label htmlFor="lastName">Last Name</label>
           <input
             type="text"
+            id="lastName"
             name="lastName"
             {...register('lastName', { required: true })}
           />
           {errors.lastName && <span className="error">This field is required</span>}
         </div>
 
+        {/* Email ID */}
         <div className="form-group">
-          <label>Email ID</label>
+          <label htmlFor="email">Email ID</label>
           <input
-            type="text"
+            type="email"
+            id="email"
             name="email"
             {...register('email', {
               required: true,
-              pattern: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
+              pattern: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
             })}
           />
           {errors.email && <span className="error">Please enter a valid email</span>}
         </div>
 
+        {/* Username */}
         <div className="form-group">
-          <label>Username</label>
+          <label htmlFor="username">Username</label>
           <input
             type="text"
+            id="username"
             name="username"
             {...register('username', { required: true })}
           />
           {errors.username && <span className="error">This field is required</span>}
+          {errorMessage && <p className="error-message">{errorMessage}</p>} {/* Display error message */}
         </div>
 
-        <div className="form-group">
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            {...register('password', { required: true })}
-          />
-          {errors.password && <span className="error">This field is required</span>}
-        </div>
-
-        <button type="submit" className="submit-button">Register</button>
+        {/* Submit Button */}
+        <button type="submit" className="submit-button">
+          Register
+        </button>
+        {registrationMessage && <p>{registrationMessage}</p>} {/* Display message */}
       </form>
-    
-
+      
     </div>
   );
 };
